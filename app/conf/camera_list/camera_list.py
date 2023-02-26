3271b4ee9dcf527874bd02ae15656a0d41cf16a9
dzming-git <dzm_work@163.com>
2023-02-26 13:48:09 +0800

    DBot_monitor

---
+++ app/conf/camera_list/camera_list.py
@@ -0,0 +1,23 @@
+import yaml
+
+class CameraList:
+    _camera_list = {}
+    _img_save_dir = ""
+
+    @classmethod
+    def _load_config(cls, config_path):
+        with open(config_path, 'r', encoding='utf-8') as f:
+            data = yaml.safe_load(f)
+            cls._camera_list = data['camera_list']
+            cls._img_save_dir = data['path_conf']['img_save_dir']
+    
+    @classmethod
+    def get_camera_by_hotkey(cls, hotkey):
+        for camera in cls._camera_list:
+            if hotkey in camera.get('hotkeys', []):
+                return (camera['ip'], camera['username'], camera['password'])
+        return None
+
+    @classmethod
+    def get_img_save_dir(cls):
+        return cls._img_save_dir
---
+++ app/message_handler/func_dict.py
@@ -0,0 +1,6 @@
+from app.monitor_server.message_handler.message_handler import send_current_camera_image, send_camera_list
+
+func_dict = {
+    '#调取监控': lambda gid=None, qid=None, msg_list=[]: send_current_camera_image(gid, qid, msg_list),
+    '#监控列表': lambda gid=None, qid=None, msg_list=[]: send_camera_list(gid, qid, msg_list)
+}
