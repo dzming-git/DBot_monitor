dd239f53b0a224c1c7c06323ac5240630cca20ac
dzming-git <dzm_work@163.com>
2023-03-03 22:24:15 +0800

    区分配置信息的来源（读取、下载），重新读取配置时根据配置信息变化决定是否重启app（条件待完善）

--- conf/route_info/route_info.py
+++ conf/route_info/route_info.py
@@ -1,58 +1,66 @@
-# route_info.yaml
+# route_info.py
 import yaml
 from utils.watch_config import watch_config
+import copy
+from utils.compare_dict import compare_dicts
 
 class RouteInfo:
     _config_path = ''
+    _config = {}
     _service_conf = {}
-    _api_gateway_conf = {
-        'find': False
-    }
-    _message_broker_conf = {
-        'find': False
-    }
+    _api_gateway_find = False
+    _message_broker_find = False
+    _api_gateway_conf_from_file = {}
+    _api_gateway_conf_from_consul = {}
+    _message_broker_conf_from_file = {}
+    _message_broker_conf_from_consul = {'endpoints': {}}
 
     @classmethod
     def load_config(cls, config_path, reload_flag=False):
         with open(config_path, 'r', encoding='utf-8') as f:
-            config = yaml.safe_load(f)
-            cls._service_conf = config.get('service', {})
-            cls._api_gateway_conf = config.get('api_gateway', {})
-            cls._message_broker_conf = config.get('message_broker', {})
+            cls._config = yaml.safe_load(f)
+            cls._service_conf = cls._config.get('service', {})
+            cls._api_gateway_conf_from_file = cls._config.get('api_gateway', {})
+            cls._message_broker_conf_from_file = cls._config.get('message_broker', {})
             if not reload_flag:
                 cls._config_path = config_path
                 watch_config(config_path, cls.reload_config)
 
     @classmethod
     def reload_config(cls):
+        config_old = copy.deepcopy(cls._config)
         cls.load_config(config_path=cls._config_path, reload_flag=True)
-
+        config_new = copy.deepcopy(cls._config)
+        added_dict, deleted_dict, modified_dict = compare_dicts(config_old, config_new)
+        if added_dict or deleted_dict or modified_dict:
+            from app.app import monitor_server_thread
+            monitor_server_thread.restart()
 
     # 机器人API网关配置方法
     @classmethod
     def get_api_gateway_name(cls):
-        return cls._api_gateway_conf.get('name')
+        return cls._api_gateway_conf_from_file.get('name')
     
     @classmethod
     def is_api_gateway_find(cls):
-        return cls._api_gateway_conf.get('find')
+        return cls._api_gateway_find
     
     @classmethod
     def update_api_gateway(cls, ip, port):
-        cls._api_gateway_conf['find'] = True
-        cls._api_gateway_conf['ip'] = ip
-        cls._api_gateway_conf['port'] = port
+        cls._api_gateway_find = True
+        cls._api_gateway_conf_from_consul['ip'] = ip
+        cls._api_gateway_conf_from_consul['port'] = port
     
     @classmethod
     def get_api_gateway_ip(cls):
-        if cls._api_gateway_conf.get('find'):
-            return cls._api_gateway_conf.get('ip')
+        if cls._api_gateway_find:
+            return cls._api_gateway_conf_from_consul.get('ip')
         return None
     
     @classmethod
     def get_api_gateway_port(cls):
-        if cls._api_gateway_conf.get('find'):
-            return cls._api_gateway_conf.get('port')
+        if cls._api_gateway_find:
+            return cls._api_gateway_conf_from_consul.get('port')
         return None
 
     # 服务程序配置方法
@@ -83,38 +91,38 @@ class RouteInfo:
     # 消息代理配置方法
     @classmethod
     def get_message_broker_name(cls):
-        return cls._message_broker_conf.get('name')
+        return cls._message_broker_conf_from_file.get('name')
     
     @classmethod
     def is_message_broker_find(cls):
-        return cls._message_broker_conf.get('find')
+        return cls._message_broker_find
     
     @classmethod
     def update_message_broker(cls, ip, port):
-        cls._message_broker_conf['find'] = True
-        cls._message_broker_conf['ip'] = ip
-        cls._message_broker_conf['port'] = port
+        cls._message_broker_find = True
+        cls._message_broker_conf_from_consul['ip'] = ip
+        cls._message_broker_conf_from_consul['port'] = port
     
     @classmethod
     def get_message_broker_ip(cls):
-        if cls._message_broker_conf.get('find'):
-            return cls._message_broker_conf.get('ip')
+        if cls._message_broker_find:
+            return cls._message_broker_conf_from_consul.get('ip')
         return None
     
     @classmethod
     def get_message_broker_port(cls):
-        if cls._message_broker_conf.get('find'):
-            return cls._message_broker_conf.get('port')
+        if cls._message_broker_find:
+            return cls._message_broker_conf_from_consul.get('port')
         return None
     
     @classmethod
     def add_message_broker_endpoint(cls, usage, endpoint):
-        cls._message_broker_conf['endpoints'][usage] = endpoint
+        cls._message_broker_conf_from_consul['endpoints'][usage] = endpoint
     
     @classmethod
     def get_message_broker_endpoint(cls, usage):
-        return cls._message_broker_conf.get('endpoints')[usage]
+        return cls._message_broker_conf_from_consul.get('endpoints')[usage]
     
     @classmethod
     def get_message_broker_consul_key(cls, usage):
-        return cls._message_broker_conf.get('consul_key')[usage]
+        return cls._message_broker_conf_from_file.get('consul_key')[usage]
